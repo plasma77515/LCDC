35180384995c1439c5717d99c41ce5997afb4055
plasma77515 <plasma77515@gmail.com>
2014-05-31 21:14:45 +0200

    Avancement
    Signed-off-by: plasma77515 <plasma77515@gmail.com>

---
+++ files/nav.php
@@ -0,0 +1,19 @@
+<div class="navbar navbar-inverse">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#"><img src="img/logo.png" width="32px">La Cité dans le Ciel</a>
+  </div>
+  <div class="navbar-collapse collapse navbar-inverse-collapse">
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="participants.php">Participants</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="post.php">Participer à l'aventure !</a></li>
+    </ul>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -8,50 +8,22 @@ require_once 'files/bdd.php';
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
-  <div class="navbar navbar-inverse">
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">Brand</a>
+  <?php require_once 'files/nav.php'; ?>
+
+  <div class="container">
+    <div class="row">
+
+      <?php require_once 'files/right.php'; ?>
+
+    <div class="panel panel-success">
+      <div class="panel-heading">tttt</div>
+      <div class="panel-content">fdfd</div>
+      <div class="panel-footer">kkk</div>
+    </div>
+    </div>
+    </div>
   </div>
-  <div class="navbar-collapse collapse navbar-inverse-collapse">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Active</a></li>
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li class="dropdown-header">Dropdown header</li>
-          <li><a href="#">Separated link</a></li>
-          <li><a href="#">One more separated link</a></li>
-        </ul>
-      </li>
-    </ul>
-    <form class="navbar-form navbar-left">
-      <input type="text" class="form-control col-lg-8" placeholder="Search">
-    </form>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
-</div>
-<?php require_once 'js/down.php'; ?>
+
+  <?php require_once 'js/down.php'; ?>
 </body>
 </html>
